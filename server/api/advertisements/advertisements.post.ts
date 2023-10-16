74c100842d2c412d94882f700bcbd96d0a55246e
ronaldo <ronaldo@neatemys.fr>
2023-10-16 17:13:24 +0200

    merge conflict fixed

--- server/api/advertisements/advertisements.post.ts
+++ server/api/advertisements/advertisements.post.ts
@@ -1,24 +1,15 @@
 import {prismaClient} from '../prismaClient'
 
 export default defineEventHandler(async(event) => {
-<<<<<<< HEAD
-    const {title, description, companyId, type} = await readBody(event)
-
-    if (!(title && description && companyId && type)) {
-=======
     const {title, description, companyId, jobId} = await readBody(event)
 
     if (!(title && description && companyId && jobId)) {
->>>>>>> origin/nuxt-server
         throw createError({statusCode: 401, message : "first name or last name or email or password is required"})
     }
 
     try {
         let advertisement = null
         await  prismaClient.advertisement.create({
-<<<<<<< HEAD
-            data: {title, description, advertisementId: companyId, type}
-=======
             data: {
                 title,
                 description,
@@ -30,7 +21,6 @@ export default defineEventHandler(async(event) => {
             include : {
                 jobs : true
             }
->>>>>>> origin/nuxt-server
         }).then((res) => {
             advertisement = res
         })
