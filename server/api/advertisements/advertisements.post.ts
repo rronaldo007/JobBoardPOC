9dc0f04e6218a0ea4b2a99630252f301f96a91fb
ronaldo <ronaldo@neatemys.fr>
2023-10-16 17:09:17 +0200

    fixed many to many job-adv

--- server/api/advertisements/advertisements.post.ts
+++ server/api/advertisements/advertisements.post.ts
@@ -1,16 +1,26 @@
 import {prismaClient} from '../prismaClient'
 
 export default defineEventHandler(async(event) => {
-    const {title, description, companyId, type} = await readBody(event)
+    const {title, description, companyId, jobId} = await readBody(event)
 
-    if (!(title && description && companyId && type)) {
+    if (!(title && description && companyId && jobId)) {
         throw createError({statusCode: 401, message : "first name or last name or email or password is required"})
     }
 
     try {
         let advertisement = null
         await  prismaClient.advertisement.create({
-            data: {title, description, advertisementId: companyId, type}
+            data: {
+                title,
+                description,
+                advertisementId: companyId,
+                jobs: {
+                    connect :[{id : jobId}]
+                }
+            },
+            include : {
+                jobs : true
+            }
         }).then((res) => {
             advertisement = res
         })
